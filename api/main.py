d3860fd8027c87e325e2687ca2e70c10b76f5f50
SergeyKalutsky <skalutsky@gmail.com>
2021-08-01 15:08:25 +0300

    added session

--- api/main.py
+++ api/main.py
@@ -3,27 +3,30 @@ from fastapi import FastAPI, Request
 
 app = FastAPI()
 
+players_lst = []
+
+
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
 
-@app.get("/ip_check")
-def read_root(request: Request):
-    client_host = request.client.host
-    print(client_host)
-    return {"client_host": client_host}
+@app.get("/players")
+def players():
+    return {"playes": players_lst}
 
 
 @app.get("/session")
-def session():
-    return {"Hello": "World"}
-
+def session(request: Request):
+    ip_adess = request.client.host
+    if ip_adess not in players_lst and len(players_lst) < 2:
+        players_lst.append(ip_adess)
+        return 'added'
+    return 'Уже слишком много игроков или вы в сессии'
 
 
 # Создать сессию(2 игрока)
 # Если сесси нет, она создается
 # Если есть и только 1 игрок, то подключается
 # Только когда все игроки готовы
-# 
-
+#
